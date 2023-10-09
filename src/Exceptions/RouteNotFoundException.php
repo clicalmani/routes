adfe9b03970c8ae8598c7cbadfcc25a5b89e6110
honorable85 <honorable85@gmail.com>
2023-10-09 12:48:12 +0100

    Console routing support

--- src/Exceptions/RouteNotFoundException.php
+++ src/Exceptions/RouteNotFoundException.php
@@ -4,6 +4,10 @@ namespace Clicalmani\Routes\Exceptions;
 class RouteNotFoundException extends \Exception {
 	function __construct($route = ''){
 		parent::__construct("Route $route Not Found");
+
+		if ( file_exists( root_path('/public/' . $_SERVER['REQUEST_URI']) ) ) {
+			header('Location: /public/' . $_SERVER['REQUEST_URI']); exit;
+		} else http_response_code(404);
 	}
 }
 ?>
